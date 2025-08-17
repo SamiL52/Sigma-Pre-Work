4fd3077f1a2f1b348ef46d1e81903a87b4be18da
SamiL52 <samilachqar@gmail.com>
2025-08-17 04:22:05 +0100

    Fixed strong typing errors from Pylance

--- age.py
+++ age.py
@@ -1,30 +1,32 @@
-from datetime import datetime
+from datetime import datetime, date
 
 
-def calculate_age_in_years(input_date: str) -> int:
-        # Validate input string by trying to convert to date object
-        try:
-            input_date_converted: datetime.date = datetime.strptime(input_date, "%d-%m-%Y").date()
-        except:
-            print("Invalid input.")
-            return None
-        
-        today: datetime.date = datetime.today().date()
-        age_in_years: int = today.year - input_date_converted.year
+def calculate_age_in_years(input_date: str) -> int | None:
+    # Validate input string by trying to convert to date object
+    try:
+        input_date_converted: date = datetime.strptime(
+            input_date, "%d-%m-%Y").date()
+    except:
+        print("Invalid input.")
+        return None
 
-        # If we haven't reached the birthday this year, subtract a year from total
-        if (today.month, today.day) < (input_date_converted.month, input_date_converted.day):
-             age_in_years -= 1
+    today: date = datetime.today().date()
+    age_in_years: int = today.year - input_date_converted.year
 
-        return age_in_years
+    # If we haven't reached the birthday this year, subtract a year from total
+    if (today.month, today.day) < (input_date_converted.month, input_date_converted.day):
+        age_in_years -= 1
+
+    return age_in_years
 
 
 def main() -> None:
     while True:
-        age_in_years: int = calculate_age_in_years(input("Enter a date (dd-mm-yyyy): "))
+        age_in_years: int | None = calculate_age_in_years(
+            input("Enter a date (dd-mm-yyyy): "))
         if age_in_years:
-             break
-    
+            break
+
     print(age_in_years)
 
 
